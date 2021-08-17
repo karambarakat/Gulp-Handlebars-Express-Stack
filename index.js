a4a1a0dcb0fc63fac725bb94e735545b314e874d
karamBarakat <karam.barakat.99@gmail.com>
2021-08-17 12:00:02 +0800

    prepare for deploying on github.io

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-app.use(express.static(path.join(__dirname, "dist")));
+// app.use(express.static(path.join(__dirname, "dist")));
 
 app.use("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "dist", "index.html"));
+  res.send("helo");
 });
 
 app.listen(process.env.PORT || 8800, () => {
